36b0bcfd4957f0120cc8b3c479964f5ed2eea20b
vagrant <vagrant@homestead>
2018-02-16 12:30:58 +0000

    Move user model to models folder

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\User;
+use App\Models\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
@@ -21,7 +21,7 @@ class RegisterController extends Controller
     */
 
     use RegistersUsers;
-    
+
 
     /**
      * Where to redirect users after registration.
@@ -63,7 +63,7 @@ class RegisterController extends Controller
      * Create a new user instance after a valid registration.
      *
      * @param  array  $data
-     * @return \App\User
+     * @return \App\Models\User
      */
     protected function create(array $data)
     {
--- app/Models/User.php
+++ app/Models/User.php
@@ -1,6 +1,7 @@
 <?php
 
-namespace App;
+namespace App\Models;
+//namespace App;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
