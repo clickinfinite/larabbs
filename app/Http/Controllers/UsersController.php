fe7a44cd98e632f757b9ec3d1266fbb3876ce631
vagrant <vagrant@homestead>
2018-02-16 15:26:48 +0000

    Only authorized user can edit user profile

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -9,25 +9,33 @@ use App\Handlers\ImageUploadHandler;
 class UsersController extends Controller
 {
     //
+    public function __construct()
+    {
+    $this->middleware('auth', ['except' => ['show']]);
+    }
     public function show(User $user)
     {
         return view('users.show', compact('user'));
     }
     public function edit(User $user)
-   {
-    return view('users.edit', compact('user'));
-   }
-   public function update(UserRequest $request, ImageUploadHandler $uploader, User $user)
-   {
-       $data = $request->all();
+    {
+        $this->authorize('update', $user);
+        return view('users.edit', compact('user'));
+    }
 
-       if ($request->avatar) {
-               $result = $uploader->save($request->avatar, 'avatars', $user->id, 362);           if ($result) {
-               $data['avatar'] = $result['path'];
-           }
-       }
+    public function update(UserRequest $request, ImageUploadHandler $uploader, User $user)
+    {
+        $this->authorize('update', $user);
+        $data = $request->all();
 
-       $user->update($data);
-       return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功！');
-   }
+        if ($request->avatar) {
+            $result = $uploader->save($request->avatar, 'avatars', $user->id, 362);
+            if ($result) {
+                $data['avatar'] = $result['path'];
+            }
+        }
+
+        $user->update($data);
+        return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功！');
+    }
 }
