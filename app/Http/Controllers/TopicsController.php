76f3430507afa5b7ff509640a4e58745809d2284
vagrant <vagrant@homestead>
2018-02-18 10:06:07 +0000

    Topic detail page

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -23,11 +23,10 @@ class TopicsController extends Controller
     return view('topics.index', compact('topics'));
 	}
 
-    public function show(Topic $topic)
-    {
-        return view('topics.show', compact('topic'));
-    }
-
+  public function show(Topic $topic)
+  {
+      return view('topics.show', compact('topic'));
+  }
 	public function create(Topic $topic)
 	{
     $categories = Category::all();
@@ -40,7 +39,8 @@ class TopicsController extends Controller
       $topic->user_id = Auth::id();
       $topic->save();
 
-      return redirect()->route('topics.show', $topic->id)->with('message', 'Created successfully.');
+    //  return redirect()->route('topics.show', $topic->id)->with('message', 'Created successfully.');
+    return redirect()->route('topics.show', $topic->id)->with('success', '成功创建主题！');
   }
 
 	public function edit(Topic $topic)
@@ -54,16 +54,18 @@ class TopicsController extends Controller
 		$this->authorize('update', $topic);
 		$topic->update($request->all());
 
-		return redirect()->route('topics.show', $topic->id)->with('message', 'Updated successfully.');
-	}
+	//	return redirect()->route('topics.show', $topic->id)->with('message', 'Updated successfully.');
+    return redirect()->route('topics.show', $topic->id)->with('success', '更新成功！');
+  }
 
 	public function destroy(Topic $topic)
 	{
 		$this->authorize('destroy', $topic);
 		$topic->delete();
 
-		return redirect()->route('topics.index')->with('message', 'Deleted successfully.');
-	}
+	//	return redirect()->route('topics.index')->with('message', 'Deleted successfully.');
+    return redirect()->route('topics.index')->with('success', '成功删除！');
+  }
   public function uploadImage(Request $request, ImageUploadHandler $uploader)
   {
     // 初始化返回数据，默认是失败的
